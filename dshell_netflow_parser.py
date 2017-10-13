155618b35b33081c1829488b58d65eeb4f6aa45c
Luis Rosario <luis.rosario@verizon.com>
2017-10-13 18:06:45 +0000

    fix bug

--- dshell_netflow_parser.py
+++ dshell_netflow_parser.py
@@ -45,7 +45,7 @@ def run_tool(path):
                 try:
                     data["date"] = fields[0].strip()
                     data["time"] = fields[1].strip()
-                    data['date_time'] = " ".join([data["date"], data['time'])
+                    data['date_time'] = " ".join([data["date"], data['time']])
                     data["src_ip"] = fields[2].strip()
                     data["dst_ip"] = fields[4].strip()
                     data["src_country_code"] = fields[5].strip()[1:]
